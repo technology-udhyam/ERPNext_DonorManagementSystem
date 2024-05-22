24b1cf57dc0ab60c3b06ee1259817446718bd6fb
Ajith B M <ajithbm@Ajiths-MacBook-Pro.local>
2024-05-22 15:21:40 +0530

    This is the final commit for Udhyam Donor Management System. This also includes APIs for accepting data from Udhyam Website donation.

---
+++ donor_management/donor_management/api.py
@@ -0,0 +1,46 @@
+import frappe
+@frappe.whitelist(allow_guest=True)
+
+def update_donor_donation_details(data):
+    try:
+        update_donor_details(data)
+        return {
+            "success": "Data Imported Successfully and Saved as Draft."
+        }
+    except Exception as e:
+        frappe.log_error("Donor API request Failed :"+ str(e))
+        
+        return {
+            "Error" : "Something went wrong!"
+        }
+
+def update_donor_details(data):
+    existing_donor = frappe.get_all("Donor", filters={"donor_name": data['donor_name'], "email": data['email']})
+    if existing_donor:
+        donor = frappe.get_doc("Donor", existing_donor[0].name)
+        create_donor_donation(donor, data)
+    else:
+        donor = frappe.new_doc("Donor")
+        create_donor_donation(donor, data)
+
+def create_donor_donation(donor, data):
+    donor.donor_name = data.donor_name
+    donor.pan_card = data.pan_card
+    donor.phone = data.phone
+    donor.address = data.address
+    donor.email = data.email
+    donor.save(ignore_permissions=True)
+    create_donation(data, donor)
+
+def create_donation(data, donor):
+    donation = frappe.new_doc("Donation")
+    donation.update({
+        "doctype": "Donation",
+        "donor_id": donor.name,
+        "date_of_donation": data['date_of_donation'],
+        "tranche_amount": data['donation_amount']
+    })
+
+    donation.insert(ignore_permissions=True)
+
+    
