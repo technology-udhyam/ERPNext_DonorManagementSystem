0670f028224ea53f385dfbdff2102e387c869370
Ramesh Krishnamoorthy <ramesh@udhyam.org>
2024-10-03 16:29:23 +0530

    Start date, End date added to Project Details. Donation will get these dates automatically. Donation submit validation added for Receipt generation. Progress report made as an attachment in Utilization

--- donor_management/donor_management/doctype/donation/donation.js
+++ donor_management/donor_management/doctype/donation/donation.js
@@ -7,9 +7,9 @@ frappe.ui.form.on("Donation", {
         // Check if date of donation is provided
         if (!date_of_donation) {
             // Throw an error and clear the fields
-            frappe.msgprint(__("Date of Donation is required."));
-            frm.set_value("preferred_currency", "");
-            frm.set_value("amount_in_preferred_currency", "");
+            frappe.msgprint(__("Date of Donation is required to calculate the exchange rate"));
+            //frm.set_value("preferred_currency", "");
+            //frm.set_value("amount_in_preferred_currency", "");
             return;
         }
 
@@ -23,12 +23,48 @@ frappe.ui.form.on("Donation", {
             callback: function(r) {
                 if (r.message) {
                     // Update the exchanged_amount field based on the fetched exchange rate
-                    var exchangeRate = r.message[0];
+                    var amountInINR = r.message[0];
                     var exchange_rate = r.message[1]
-                    frm.set_value('tranche_amount', exchangeRate);
+                    frm.set_value('tranche_amount', amountInINR);
                     frm.set_value('exchange_rate', exchange_rate);
                 }
             }
         });
+    },
+    date_of_donation: function(frm) {
+        var preferred_currency = frm.doc.preferred_currency;
+        var amount_in_preferred_currency = frm.doc.amount_in_preferred_currency;
+        var date_of_donation = frm.doc.date_of_donation;
+
+        // Check if date of donation is provided
+        if (date_of_donation && amount_in_preferred_currency) {
+
+        frappe.call({
+            method: 'donor_management.donor_management.doctype.donation.donation.exchange_rate',
+            args: {
+                preferred_currency: preferred_currency,
+                amount_in_preferred_currency: amount_in_preferred_currency,
+                date_of_donation: date_of_donation
+            },
+            callback: function(r) {
+                if (r.message) {
+                    // Update the exchanged_amount field based on the fetched exchange rate
+                    var amountInINR = r.message[0];
+                    var exchange_rate = r.message[1]
+                    frm.set_value('tranche_amount', amountInINR);
+                    frm.set_value('exchange_rate', exchange_rate);
+                }
+            }
+        });
+	}
+    },
+    before_submit: function(frm, cdt, cdn) {
+	var receipt_generated = frm.doc.receipt_generated;
+	if (receipt_generated=='No') {
+		frappe.validated=false;
+		frappe.msgprint(__("Please submit ONLY after all formalities are completed including receipt generation"));
+		return false;
+	}
+	return true;
     },
 });
