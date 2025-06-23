a3c0ce665cfe8170ad041accecdcbc8fa5bee490
Ramesh Krishnamoorthy <ramesh@udhyam.org>
2025-06-23 20:49:53 +0530

    Proposal doctype added

---
+++ donor_management/donor_management/doctype/proposal/proposal.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Tech4Good Community and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Proposal", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ donor_management/donor_management/doctype/proposal/test_proposal.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Tech4Good Community and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestProposal(FrappeTestCase):
+	pass
